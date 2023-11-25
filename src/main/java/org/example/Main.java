fa431d1bf311c729a4231be29740e6045449735f
Qiaomu Lei <115581793+RayQM@users.noreply.github.com>
2023-11-25 11:21:43 -0700

    String Reverser
    done!

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        String str = "abcd";
+
+        StringReverser reverser = new StringReverser();
+
+        var result = reverser.reverse(str);
+
+        System.out.println(result);
+
+    }
+}
